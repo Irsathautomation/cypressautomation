b728c7c275ef60b713e4e3e0ea95b58855010d8f
Irsathautomation <irsathahamed2022@gmail.com>
2024-04-17 22:05:22 +0530

    API

---
+++ cypress/e2e/API.cy.js
@@ -0,0 +1,28 @@
+/// <reference types="Cypress" />
+
+describe('My First Test Suite', function() 
+{
+it('My FirstTest case',function() {
+    //Get
+    cy.request('GET','https://restful-booker.herokuapp.com/booking').then((res)=>{
+        expect(res.status).to.eq(200)
+        cy.log(res.body)
+    })
+    //POST
+    cy.request('POST','https://restful-booker.herokuapp.com/booking',{
+        "firstname" : "Jim",
+        "lastname" : "Brown",
+        "totalprice" : 111,
+        "depositpaid" : true,
+        "bookingdates" : {
+            "checkin" : "2018-01-01",
+            "checkout" : "2019-01-01"
+        },
+        "additionalneeds" : "Breakfast"
+    }).then((res)=>{
+        cy.log(res.body)
+        cy.log(res.statusText)
+    })
+
+
+})})
--- cypress/e2e/amazonintercept.cy.js
+++ cypress/e2e/amazonintercept.cy.js
@@ -1,27 +1,37 @@
-/// <reference types = "cypress" /> 
-
-import { each } from "cypress/types/bluebird";
-import { forEach } from "cypress/types/lodash";
-
-describe("Login spec", () => {
-  beforeEach("visit webpage", () => {
-    //visits amazon website
-    cy.visit("https://www.amazon.in");
-  });
-
-  afterEach("Everylist", () => {
-    //clearing cache
-    cy.clearCookies();
-
-  });
-  it('amazonapi',()=>{
+/// <reference types="Cypress" />
+import data from "..\\..\\cypress\\fixtures\\Addtocart.json" 
+describe('My First Test Suite', function() 
+{
+it('My FirstTest case',function() {
+    cy.visit("https://www.amazon.in/");
     cy.intercept({
-        method : "POST",
-        url:"https://unagi-eu.amazon.com/1/events/com.amazon.eel.amazonlive.metrics.nexus"
-    },{
-        statusCode:200,
-        body:{}
-    })
-    cy.get("div .as-title-block-left > span")
-  })
+        method : 'HEAD',
+        url : 'https://completion.amazon.in/api/2017/suggestions'
+    },
+     {
+         statusCode : 204,
+     }).as('Randomapi')
+     cy.get("div > input[class='nav-input nav-progressive-attribute']").as("input").click().then(()=>{
+      cy.get('@input').type(data.Type)
+      cy.get("#nav-search-submit-button").click();
+     })
+     cy.wait('@Randomapi').then(({request,response})=>
+     {
+      cy.get(".a-checkbox-fancy >label>input[type='checkbox']").first().as("check").then(()=>{
+        cy.get('@check').next().click();
+      })
+     })
+     
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+})
+ 
 })
